ae391bad8e6cf3dd84b1f09cb6dcb46490f633f6
Dixit <Shashank.Dixit@philips.com>
2020-09-13 19:22:36 +0530

    added test projects

---
+++ EnvironmentMonitoringSystem/sender.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace sender.Tests
+{
+    
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
