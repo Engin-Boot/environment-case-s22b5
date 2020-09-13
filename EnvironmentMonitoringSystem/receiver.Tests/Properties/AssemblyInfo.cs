a254127a5b56c321af5d835f998122b2649ffc99
Dixit <Shashank.Dixit@philips.com>
2020-09-13 20:19:06 +0530

    added test project

---
+++ EnvironmentMonitoringSystem/receiver.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("receiver.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("receiver.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5df41e3c-dd77-4e29-a5a5-1634150471a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EnvironmentMonitoringSystem/sender.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace sender.Tests
+{
+    
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
