ecc0a6969b6340a42682803717410888fc0ca5a6
Dixit <Shashank.Dixit@philips.com>
2020-09-15 01:14:29 +0530

    resharper refactoring

---
+++ receiver/Utils/ReceiverConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace receiver.Utils
+{
+    internal static class ReceiverConstants
+    {
+        public const int Ok = 0;
+        public const int Warning = 1;
+        public const int Error = 2;
+        public const int TemperatureErrorLowerLimit = 0;
+        public const int TemperatureErrorUpperLimit = 40;
+        public const int TemperatureOkLowerLimit = 4;
+        public const int TemperatureOkUpperLimit = 37;
+        public const int HumidityOkUpperLimit = 70;
+        public const int HumidityErrorLowerLimit = 90;
+    }
+}
