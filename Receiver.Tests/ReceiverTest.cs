a67f58a26ffcd59d7f4a5a099d0ae5436331c4e2
Dixit <Shashank.Dixit@philips.com>
2020-09-15 19:01:08 +0530

    added unit test cases for temperature rangeChecker

--- Receiver.Tests/ReceiverTest.cs
+++ Receiver.Tests/ReceiverTest.cs
@@ -4,49 +4,49 @@ namespace Receiver.Tests
 {
     public class ReceiverTest
     {
-        private readonly RangeChecker rangeChecker = new RangeChecker();
+        private readonly RangeChecker _rangeChecker = new RangeChecker();
         [Fact]
         public void WhenTemperatureIsInNormalRangeThenReturnOkStatusCode()
         {
             
             var temperature = 20;
-            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
+            Assert.True(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
             temperature = 37;
-            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
+            Assert.False(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
             temperature = 42;
-            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
+            Assert.False(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
             temperature = -2;
-            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
+            Assert.False(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
         }
         [Fact]
         public void WhenTemperatureIsInWarningRangeThenReturnWarningStatusCode()
         {
             var temperature = 4;
-            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            Assert.True(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
             temperature = 2;
-            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            Assert.True(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
             temperature = 0;
-            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            Assert.False(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
             temperature = 30;
-            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            Assert.False(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
             temperature = 37;
-            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            Assert.True(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
             temperature = 40;
-            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            Assert.False(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
         }
         [Fact]
         public void WhenTemperatureIsInErrorRangeThenReturnErrorStatusCode()
         {
             var temperature = 0;
-            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+            Assert.True(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
             temperature = 2;
-            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+            Assert.False(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
             temperature = 40;
-            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+            Assert.True(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
             temperature = 30;
-            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+            Assert.False(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
             temperature = 37;
-            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+            Assert.False(_rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
         }
     }
 }
