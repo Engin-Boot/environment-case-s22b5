f46ff332c7e677b9fab7c9a3e73be7bfaf00dfb6
Dixit <Shashank.Dixit@philips.com>
2020-09-15 18:43:47 +0530

    added unit test cases for temperature rangeChecker

---
+++ Receiver.Tests/ReceiverTest.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using receiver.Utils;
+namespace Receiver.Tests
+{
+    public class ReceiverTest
+    {
+        private readonly RangeChecker rangeChecker = new RangeChecker();
+        [Fact]
+        public void WhenTemperatureIsInNormalRangeThenReturnOkStatusCode()
+        {
+            
+            var temperature = 20;
+            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
+            temperature = 37;
+            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
+            temperature = 42;
+            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
+            temperature = -2;
+            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 0);
+        }
+        [Fact]
+        public void WhenTemperatureIsInWarningRangeThenReturnWarningStatusCode()
+        {
+            var temperature = 4;
+            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            temperature = 2;
+            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            temperature = 0;
+            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            temperature = 30;
+            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            temperature = 37;
+            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+            temperature = 40;
+            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 1);
+        }
+        [Fact]
+        public void WhenTemperatureIsInErrorRangeThenReturnErrorStatusCode()
+        {
+            var temperature = 0;
+            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+            temperature = 2;
+            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+            temperature = 40;
+            Assert.True(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+            temperature = 30;
+            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+            temperature = 37;
+            Assert.False(rangeChecker.CheckTemperatureAndReturnStatusCode(temperature) == 2);
+        }
+    }
+}
