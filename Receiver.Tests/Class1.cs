f92d3aaece7f9ce140b98279d69841ab5afb4615
Dixit <Shashank.Dixit@philips.com>
2020-09-15 17:40:42 +0530

    added test projects

---
+++ Receiver.Tests/Class1.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace Receiver.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void TemtMethod1() { }
+    }
+}
---
+++ Sender.Tests/Class1.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+
+namespace Sender.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestMethod1()
+        { }
+    }
+}
